db8ca205b6ed8fa59522b2ddddf72474b95fdfb6
ayhanakkaya <ayhanakkayameb@gmail.com>
2023-11-13 16:57:08 +0300

    Veritabanı ve sınıflar oluştu

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-OVJEIF5;database=CoreBlogDb;integrated security=true;");
+        }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+
+    }
+}
